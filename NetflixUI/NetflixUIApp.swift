4105eda674194786192cf6a14326e3ee0e74cfdf
Tish <tish302@gmail.com>
2021-07-29 09:48:18 -0600

    Initial Commit

---
+++ NetflixUI/NetflixUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUIApp.swift
+//  NetflixUI
+//
+//  Created by Patricia Obregon on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
